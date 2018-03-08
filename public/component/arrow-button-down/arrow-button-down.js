3ab266562fd130354a8c4effd4ec1ee5969de38c
wangjianjune <wangjianjun0730@126.com>
2018-03-09 00:31:48 +0800

    组件

---
+++ public/component/arrow-button-down/arrow-button-down.js
@@ -0,0 +1,47 @@
+// public/component/arrow-button-down/arrow-button-down.js
+Component({
+  /**
+   * 关联组件
+  */
+  relations:{
+    '../navigation-bar/navigation-bar':{
+      type:'parent',
+      linked: function (target) {
+        wx.showToast({
+          title: '插入子组件',
+        })
+      },
+      linkChanged: function (target) {
+        wx.showToast({
+          title: '按钮组件变化',
+        })
+      },
+      unlinked: function (target) {
+        wx.showToast({
+          title: '按钮组件被移除',
+        })
+      }
+    }
+  },
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
---
+++ public/component/navigation-bar/navigation-bar.js
@@ -0,0 +1,63 @@
+
+Component({
+
+/**
+ * 关联向下箭头组件
+*/
+  relations:{
+  '../arrow-button-down/arrow-button-down':{
+    type:'child',
+    linked: function (target){
+      wx.showToast({
+        title: '插入了按钮子组件',
+      })
+    },
+    linkChanged: function (target){
+      wx.showToast({
+        title: '按钮组件插入了',
+      })
+    },
+    unlinked: function (target){
+      wx.showToast({
+        title: '按钮组件被移除',
+      })
+    }
+  }
+},
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+   
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    _propertyChanged:function(newValue,oldValue){
+      
+    }
+  },
+
+  /**
+   * 生命周期函数
+   */
+  //不能使用setData
+  created: function(){
+
+  }
+
+
+
+
+
+})
